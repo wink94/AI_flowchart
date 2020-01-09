64b9e0a60f8484b1610ee0fea93864aea248812f
wink94 <mwindula16@gmail.com>
2020-01-09 19:17:54 +0530

    repo clean

--- Mask RCNN/flowchart/flowchart.py
+++ Mask RCNN/flowchart/flowchart.py
@@ -35,7 +35,7 @@ import numpy as np
 import skimage.draw
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath("../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
